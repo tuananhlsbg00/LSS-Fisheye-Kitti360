094b824c3378c6674efdee0f86c2fdb6cae4e7f9
nvtalsbg00 <nvtalsbg00@gmail.com>
2025-04-03 17:16:22 +0700

    fix minor error of negative number of height bin nx[2]

--- src_fisheye/tools.py
+++ src_fisheye/tools.py
@@ -175,7 +175,7 @@ normalize_img = torchvision.transforms.Compose((
 def gen_dx_bx(xbound, ybound, zbound):
     dx = torch.Tensor([row[2] for row in [xbound, ybound, zbound]])
     bx = torch.Tensor([row[0] + row[2]/2.0 for row in [xbound, ybound, zbound]])
-    nx = torch.LongTensor([(row[1] - row[0]) / row[2] for row in [xbound, ybound, zbound]])
+    nx = torch.LongTensor([(np.abs(row[1] - row[0]) / row[2]) for row in [xbound, ybound, zbound]])
 
     return dx, bx, nx
 
